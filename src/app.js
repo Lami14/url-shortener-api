62c5b0270920fb5f8192b2aa57fbcfa576f6a980
Lami14 <123112707+Lami14@users.noreply.github.com>
2026-03-11 10:37:58 +0200

    Update app.js to serve frontend files and handle URL redirection

--- src/app.js
+++ src/app.js
@@ -1,17 +1,48 @@
+// src/app.js
+
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const urlRoutes = require("./routes/urlRoutes");
 
 dotenv.config();
+
+// connect database
 connectDB();
 
 const app = express();
 
+// middleware
 app.use(express.json());
 
+// serve static frontend files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// API routes
 app.use("/api/url", urlRoutes);
+
+// redirect route for shortened URLs
+app.get("/:code", async (req, res) => {
+  const Url = require("./models/Url");
+
+  try {
+    const url = await Url.findOne({ shortCode: req.params.code });
+
+    if (!url) {
+      return res.status(404).send("URL not found");
+    }
+
+    url.clicks++;
+    await url.save();
+
+    res.redirect(url.longUrl);
+
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
